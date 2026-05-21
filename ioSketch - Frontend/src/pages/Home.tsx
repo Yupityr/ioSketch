5102b089d0d1bbf1dbb67ac07339771a642c425f
Yupityr <jrchchrstngithub@gmail.com>
2026-05-21 22:35:38 +0800

    add: Home changes, router changes

--- ioSketch - Frontend/src/pages/Home.tsx
+++ ioSketch - Frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import About from "../components/About";
 
 const Home = () => { 
   const navigate = useNavigate();
     return (
+      <>
         <main className="min-h-screen bg-linear-to-br from-slate-950 via-slate-900 to-slate-950 flex items-center justify-center overflow-hidden">
             <div className="relative z-10 max-w-xl w-full px-6">
               <div className="backdrop-blur-xl bg-linear-to-b from-slate-800/60 to-slate-900/40 rounded-2xl border border-slate-700/50 shadow-2xl p-8 md:p-12 transition-all duration-500 hover:border-slate-600/80">
@@ -13,6 +15,8 @@ const Home = () => {
               </div>
             </div>
         </main>
+        <About />
+      </>
     )
 };
 
--- ioSketch - Frontend/src/router/router.tsx
+++ ioSketch - Frontend/src/router/router.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import DrawingCanvas from "../features/sketch/components/Drawingcanvas";
+import Homelayout from "../pages/Homelayout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />
+        element: <Homelayout />,
+        children:[
+            {index: true, element: <Home/>},
+        ]
     },
     {
         path: "/sketch",
