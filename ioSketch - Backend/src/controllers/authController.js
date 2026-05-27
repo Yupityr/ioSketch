a5e89daa04fe5aac7babf60771fa11b45279fff6
Yupityr <jrchchrstngithub@gmail.com>
2026-05-27 17:52:46 +0800

    ADD: login function, and JWT

--- ioSketch - Backend/src/controllers/authController.js
+++ ioSketch - Backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import { prisma } from "../config/db.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
 
 const register = async (req, res) => {
     const {name, email, password} = req.body;
@@ -26,6 +27,9 @@ const register = async (req, res) => {
         },
     });
 
+    //JWT Token
+    const token = generateToken(user.id, res)
+
     res.status(201).json({
         status: "success",
         data: {
@@ -33,9 +37,54 @@ const register = async (req, res) => {
                 id: user.id,
                 name: user.name,
                 email: user.email
-            }
+            },
+            token,
         }
     })
 };
 
-export {register};
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {email: email},
+    });
+
+    if (!user) {
+        return res.status(401).json({ error: "Invalid email or password"})
+    }
+
+    //verify password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(401).json({error : "Invalid email or password"})
+    }
+
+    //JWT Token
+    const token = generateToken(user.id, res)
+
+    res.status(201).json({
+        status: "Sign in successful",
+        data: {
+            user:{
+                id: user.id,
+                email: user.email
+            },
+            token,
+        }
+    });
+};
+
+const signout = async (req, res) => {
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({
+        status: "Signed out",
+        message: "Sign out successfully"
+    })
+}
+
+export {register, login, signout};
--- ioSketch - Backend/src/routes/authRoutes.js
+++ ioSketch - Backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { register, login, signout } from "../controllers/authController.js";
 
 const router = express.Router();
 
 
 router.post("/signup", register);
+router.post("/signin", login);
+router.post("/signout", signout);
 
 export default router;
